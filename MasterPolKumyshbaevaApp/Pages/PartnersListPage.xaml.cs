aca34f9192d3a8c4128f616bea7fa4361598c003
Gogs <ilivic@yandex.ru>
2025-05-21 17:04:18 +0300

    Добавьте файлы проекта.

---
+++ MasterPolKumyshbaevaApp/Pages/PartnersListPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MasterPolKumyshbaevaApp.ModelsConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterPolKumyshbaevaApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PartnersListPage.xaml
+    /// </summary>
+    public partial class PartnersListPage : Page
+    {
+        public static List<Partner> partners { get; set; }
+        public PartnersListPage()
+        {
+            InitializeComponent();
+            partners = new List<Partner>(DbConnection.masterPolEntitites.Partner.ToList());
+        
+            this.DataContext = this;
+        }
+    }
+}
